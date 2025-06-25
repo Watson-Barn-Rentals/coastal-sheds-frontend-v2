2d1efb933a7b43ab93c1947f56bf303359f87e0b
Nathaniel Rudolph <nathaniel@watsonrentals.net>
2025-06-25 11:29:07 -0500

    Fixes to preview middleware

--- server/middleware/preview.ts
+++ server/middleware/preview.ts
@@ -2,23 +2,19 @@
 import { defineEventHandler, getCookie, sendRedirect } from 'h3'
 
 export default defineEventHandler(async (event) => {
-  // only on preview deployment
-  if (process.env.PREVIEW_MODE === 'true') {
-    return
-  }
-
   const { req } = event.node
   const token = getCookie(event, 'previewToken')
 
-  // 1) whitelist the “start preview” page so you can land there without a token
-  const url = req.url || '/'
-  if (url === '/start-preview-session') {
+  if (process.env.PREVIEW_MODE !== 'true') {
     return
   }
 
-  // 2) everyone else without a token must go back to the main site
+  if (req.url === '/start-preview-session') {
+    return
+  }
+
   if (!token) {
-    sendRedirect(event, `${process.env.PRODUCTION_SITE_URL}${url}`, 302)
+    sendRedirect(event, `${process.env.PRODUCTION_SITE_URL}${req.url ?? '/'}`, 302)
     return
   }
 
