a0e80759c58cce0a862c5aed5d06de8d2fffda30
Nathaniel Rudolph <nathaniel@watsonrentals.net>
2025-09-04 07:18:28 -0500

    fix: remove unused Leadferno script integration from default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,4 @@
 
-<script lang="ts" setup>
-    useHead({
-      script: [
-        {
-          src: 'https://widget.leadferno.com/leadbox.js',
-          'data-leadferno': '',
-          'data-bid': '4435ba16-2987-4fbd-a93b-bcc263fda919',
-          'data-url': 'https://api.leadferno.com/v0'
-        }
-      ]
-    })
-</script>
 <template>
   <UApp>
       <Header />
