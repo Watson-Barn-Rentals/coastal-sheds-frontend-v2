34a8a1846066fb11b198b7115e9a874229bd1744
Nathaniel Rudolph <nathaniel@watsonrentals.net>
2026-02-11 12:03:44 -0600

    fix: correct inventory route path in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
 	ssr: !isPreviewMode,
 
   	routeRules: {
-  	  '/inventory': {
+  	  '/inventory/': {
   	    ssr: true,
   	    prerender: false,
   	    headers: {
